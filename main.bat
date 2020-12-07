08db10027220639b6cff94345c513488f3af9578
ARDELPROMASTER <up190517@alumnos.upa.edu.mx>
2020-12-07 13:18:33 -0600

    Firt version

---
+++ main.bat
@@ -0,0 +1,4 @@
+@echo off
+xcopy /c /k /y "C:\Users\jesus\Documents\UPA\Tercer cuatrimestre\Mantenimiento\Information" "C:\Users\jesus\Documents\UPA\Tercer cuatrimestre\Mantenimiento\Proyecto"
+echo Backup complet!
+@pause
